48457add71a9366856d0a2e72215b5a134ed7ce8
212011 <212011@1220-007-22>
2022-10-18 15:12:41 +0300

    Добавьте файлы проекта.

---
+++ CPFB/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using CPFB.ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPFB
+{
+    internal class DBConnection
+    {
+        public static PFBEntities2 connection = new PFBEntities2();
+    }
+}
---
+++ CPFB/Program.cs
@@ -0,0 +1,61 @@
+﻿using CPFB.ADO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPFB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var num1 in DBConnection.connection.Numbers)
+            {
+                foreach (var oper in DBConnection.connection.Operators)
+                {
+                    foreach (var num2 in DBConnection.connection.Numbers)
+                    {
+                        if(num1.Number_ID != num2.Number_ID)
+                        {
+                            Numbers number1 = new Numbers()
+                            {
+                                Number = num1.Number,
+                                Number_ID = num1.Number_ID
+                            };
+                            Numbers number2 = new Numbers()
+                            {
+                                Number = num2.Number,
+                                Number_ID = num2.Number_ID
+                            };
+                            Operators operator1 = new Operators()
+                            {
+                                Operator_ID = oper.Operator_ID,
+                                Operator = oper.Operator
+                            };
+                            string str = $"{num1.Number}{oper.Operator}{num2.Number}";
+                            double rezult = Convert.ToDouble(new DataTable().Compute(str, ""));
+                            Answer answer = new Answer()
+                            {
+                                Number_ID = number1.Number_ID,
+                                Operator_ID = operator1.Operator_ID,
+                                Answer1 = rezult
+                            };
+                            DBConnection.connection.Answer.Add(answer);
+                            DBConnection.connection.SaveChanges();
+                        }
+                    }
+                }
+            }
+            try
+            {
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
