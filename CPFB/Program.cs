9c5fe849926fe479210738804a3cd95b069b4f10
Volfone <106504367+Volfone@users.noreply.github.com>
2022-10-19 12:48:02 +0300

    Update Program.cs

--- CPFB/Program.cs
+++ CPFB/Program.cs
@@ -1,7 +1,8 @@
-﻿using CPFB.ADO;
+using CPFB.ADO;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,37 +13,21 @@ namespace CPFB
     {
         static void Main(string[] args)
         {
-            foreach (var num1 in DBConnection.connection.Numbers)
+            foreach (var num1 in DBConnection.connection.Number.ToList())
             {
-                foreach (var oper in DBConnection.connection.Operators)
+                foreach (var oper in DBConnection.connection.Operator.ToList())
                 {
-                    foreach (var num2 in DBConnection.connection.Numbers)
+                    foreach (var num2 in DBConnection.connection.Number.ToList())
                     {
                         if(num1.Number_ID != num2.Number_ID)
                         {
-                            Numbers number1 = new Numbers()
-                            {
-                                Number = num1.Number,
-                                Number_ID = num1.Number_ID
-                            };
-                            Numbers number2 = new Numbers()
-                            {
-                                Number = num2.Number,
-                                Number_ID = num2.Number_ID
-                            };
-                            Operators operator1 = new Operators()
-                            {
-                                Operator_ID = oper.Operator_ID,
-                                Operator = oper.Operator
-                            };
-                            string str = $"{num1.Number}{oper.Operator}{num2.Number}";
+                            string str = $"{num1.Number1}{oper.Operator1}{num2.Number1}";
                             double rezult = Convert.ToDouble(new DataTable().Compute(str, ""));
-                            Answer answer = new Answer()
-                            {
-                                Number_ID = number1.Number_ID,
-                                Operator_ID = operator1.Operator_ID,
-                                Answer1 = rezult
-                            };
+                            Answer answer = new Answer();
+                            answer.FNumber_ID = num1.Number_ID;
+                            answer.SNumber_ID = num2.Number_ID;
+                            answer.Operator_ID = oper.Operator_ID;
+                            answer.Answer1 = rezult;
                             DBConnection.connection.Answer.Add(answer);
                             DBConnection.connection.SaveChanges();
                         }
@@ -52,9 +37,13 @@ namespace CPFB
             try
             {
             }
-            catch
+            catch(DbUpdateException ex)
             {
-
+                Console.WriteLine(ex);
+            }
+            while(true)
+            {
+                int i = 0;
             }
         }
     }
